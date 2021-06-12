b4047e49620ab6a91c36b8f708862c1a5e50595b
m2papierz <m2papierz@gmail.com>
2021-06-12 19:02:58 +0200

    Refactored model architecture

--- cell_segmentator/unet_model.py
+++ cell_segmentator/unet_model.py
@@ -1,3 +1,6 @@
+import tensorflow as tf
+import tensorflow.keras.backend as K
+
 from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
@@ -17,7 +20,7 @@ def conv2d_block(input_tensor, n_filters, kernel_size, batch_norm):
     :param n_filters: number of filters in the convolutional layer
     :param kernel_size: kernel size of the convolutional layer
     :param batch_norm: boolean flag for setting batch normalization
-    :return: Output of two convolutional layers
+    :return: Output tensor of two convolutional layers
     """
     # First layer
     x = Conv2D(filters=n_filters, kernel_size=(kernel_size, kernel_size),
@@ -38,79 +41,80 @@ def conv2d_block(input_tensor, n_filters, kernel_size, batch_norm):
 
 class Unet:
     """
-    Class representing the deep neural network U-net architecture for medical image segmentation.
+    Standard U-net architecture.
 
+    :param img_height: height of the input image tensor
+    :param img_width: width of the input image tensor
+    :param img_channels: number of channels of the input image tensor
     :param dropout: dropout rate
     :param n_filters: base number of filters in the convolutional layers
     :param kernel_size: kernel size of the convolutional layer
     :param batch_norm: boolean flag for setting batch normalization
     """
 
-    def __init__(self, dropout=0.05, n_filters=16, kernel_size=3, batch_norm=True):
-        self.dropout = dropout
-        self.n_filters = n_filters
-        self.kernel_size = kernel_size
-        self.batch_norm = batch_norm
-
-    def build(self, img_height, img_width, img_channels):
-        """
-        Builds U-net architecture model.
-
-        :param img_height: height the input image
-        :param img_width: width of the input image
-        :param img_channels: number of channels of the input image
-        :return: Unet model
-        """
+    def __init__(self, img_height, img_width, img_channels, dropout=0.05, n_filters=16, kernel_size=3, batch_norm=True):
         # Contracting path
         input_ = Input((img_height, img_width, img_channels))
-        c1 = conv2d_block(input_tensor=input_, n_filters=self.n_filters * 1, kernel_size=self.kernel_size,
-                          batch_norm=self.batch_norm)
+        c1 = conv2d_block(input_tensor=input_, n_filters=n_filters * 1, kernel_size=kernel_size,
+                          batch_norm=batch_norm)
         p1 = MaxPooling2D((2, 2))(c1)
-        p1 = Dropout(self.dropout)(p1)
+        p1 = Dropout(dropout)(p1)
 
-        c2 = conv2d_block(input_tensor=p1, n_filters=self.n_filters * 2, kernel_size=self.kernel_size,
-                          batch_norm=self.batch_norm)
+        c2 = conv2d_block(input_tensor=p1, n_filters=n_filters * 2, kernel_size=kernel_size,
+                          batch_norm=batch_norm)
         p2 = MaxPooling2D((2, 2))(c2)
-        p2 = Dropout(self.dropout)(p2)
+        p2 = Dropout(dropout)(p2)
 
-        c3 = conv2d_block(input_tensor=p2, n_filters=self.n_filters * 4, kernel_size=self.kernel_size,
-                          batch_norm=self.batch_norm)
+        c3 = conv2d_block(input_tensor=p2, n_filters=n_filters * 4, kernel_size=kernel_size,
+                          batch_norm=batch_norm)
         p3 = MaxPooling2D((2, 2))(c3)
-        p3 = Dropout(self.dropout)(p3)
+        p3 = Dropout(dropout)(p3)
 
-        c4 = conv2d_block(input_tensor=p3, n_filters=self.n_filters * 8, kernel_size=self.kernel_size,
-                          batch_norm=self.batch_norm)
+        c4 = conv2d_block(input_tensor=p3, n_filters=n_filters * 8, kernel_size=kernel_size,
+                          batch_norm=batch_norm)
         p4 = MaxPooling2D((2, 2))(c4)
-        p4 = Dropout(self.dropout)(p4)
+        p4 = Dropout(dropout)(p4)
 
-        c5 = conv2d_block(input_tensor=p4, n_filters=self.n_filters * 16, kernel_size=self.kernel_size,
-                          batch_norm=self.batch_norm)
+        c5 = conv2d_block(input_tensor=p4, n_filters=n_filters * 16, kernel_size=kernel_size,
+                          batch_norm=batch_norm)
 
         # Expansive path
-        u6 = Conv2DTranspose(self.n_filters * 8, (3, 3), strides=(2, 2), padding='same')(c5)
+        u6 = Conv2DTranspose(n_filters * 8, (3, 3), strides=(2, 2), padding='same')(c5)
         u6 = concatenate([u6, c4])
-        u6 = Dropout(self.dropout)(u6)
-        c6 = conv2d_block(input_tensor=u6, n_filters=self.n_filters * 8, kernel_size=self.kernel_size,
-                          batch_norm=self.batch_norm)
+        u6 = Dropout(dropout)(u6)
+        c6 = conv2d_block(input_tensor=u6, n_filters=n_filters * 8, kernel_size=kernel_size,
+                          batch_norm=batch_norm)
 
-        u7 = Conv2DTranspose(self.n_filters * 4, (3, 3), strides=(2, 2), padding='same')(c6)
+        u7 = Conv2DTranspose(n_filters * 4, (3, 3), strides=(2, 2), padding='same')(c6)
         u7 = concatenate([u7, c3])
-        u7 = Dropout(self.dropout)(u7)
-        c7 = conv2d_block(input_tensor=u7, n_filters=self.n_filters * 4, kernel_size=self.kernel_size,
-                          batch_norm=self.batch_norm)
+        u7 = Dropout(dropout)(u7)
+        c7 = conv2d_block(input_tensor=u7, n_filters=n_filters * 4, kernel_size=kernel_size,
+                          batch_norm=batch_norm)
 
-        u8 = Conv2DTranspose(self.n_filters * 2, (3, 3), strides=(2, 2), padding='same')(c7)
+        u8 = Conv2DTranspose(n_filters * 2, (3, 3), strides=(2, 2), padding='same')(c7)
         u8 = concatenate([u8, c2])
-        u8 = Dropout(self.dropout)(u8)
-        c8 = conv2d_block(input_tensor=u8, n_filters=self.n_filters * 2, kernel_size=self.kernel_size,
-                          batch_norm=self.batch_norm)
+        u8 = Dropout(dropout)(u8)
+        c8 = conv2d_block(input_tensor=u8, n_filters=n_filters * 2, kernel_size=kernel_size,
+                          batch_norm=batch_norm)
 
-        u9 = Conv2DTranspose(self.n_filters * 1, (3, 3), strides=(2, 2), padding='same')(c8)
+        u9 = Conv2DTranspose(n_filters * 1, (3, 3), strides=(2, 2), padding='same')(c8)
         u9 = concatenate([u9, c1])
-        u9 = Dropout(self.dropout)(u9)
-        c9 = conv2d_block(input_tensor=u9, n_filters=self.n_filters * 1, kernel_size=self.kernel_size,
-                          batch_norm=self.batch_norm)
+        u9 = Dropout(dropout)(u9)
+        c9 = conv2d_block(input_tensor=u9, n_filters=n_filters * 1, kernel_size=kernel_size,
+                          batch_norm=batch_norm)
         output_ = Conv2D(1, (1, 1), activation='sigmoid')(c9)
 
-        model = Model(inputs=[input_], outputs=[output_])
-        return model
+        self.model = Model(inputs=[input_], outputs=[output_])
+
+    def train(self, dataset, epochs, loss_function, callbacks, optimizer="Adam", metric="accuracy"):
+        epoch_steps = dataset['train'].shape / epochs
+        val_steps = dataset['val'].shape / epochs
+
+        self.model.compile(loss=loss_function, optimizer=optimizer, metrics=[metric])
+
+        return self.model.fit(dataset['train'],
+                              steps_per_epoch=epoch_steps,
+                              validation_data=dataset['val'],
+                              validation_steps=val_steps,
+                              epochs=epochs,
+                              callbacks=callbacks)
