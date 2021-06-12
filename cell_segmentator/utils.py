086328f4aaf35cc9f85d1f0ab79aaff72eff76be
m2papierz <m2papierz@gmail.com>
2021-06-12 19:24:04 +0200

    Utilization functions for data processing

---
+++ cell_segmentator/utils.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+
+
+@tf.function
+def normalize_images(input_image: tf.Tensor, input_mask: tf.Tensor) -> tuple:
+    """
+    Normalize input tensors. Rescales images pixels values into 0.0 and 1.0 compared to [0,255].
+
+    :param input_image: input image tensor
+    :param input_mask: input mask tensor
+    :return: Normalized tensors.
+    """
+    input_image = tf.cast(input_image, tf.float32) / 255.0
+    return input_image, input_mask
+
+
+@tf.function
+def process_train_images(data: dict, image_size: int) -> tuple:
+    """
+    Augment resize images and augment by flip rotations.
+
+    :param data: dict containing an image and its mask
+    :param image_size: size of the image tensor
+    :return: Processed imaged and its mask.
+    """
+    input_image = tf.image.resize(data['image'], (image_size, image_size))
+    input_mask = tf.image.resize(data['segmentation_mask'], (image_size, image_size))
+
+    if tf.random.uniform(()) > 0.5:
+        input_image = tf.image.flip_left_right(input_image)
+        input_mask = tf.image.flip_left_right(input_mask)
+
+    input_image, input_mask = normalize_images(input_image, input_mask)
+
+    return input_image, input_mask
+
+
+@tf.function
+def process_test_images(data: dict, image_size: int) -> tuple:
+    """
+    Normalize and resize test image and mask tensors.
+
+    :param data: dict containing an image and its mask
+    :param image_size: size of the image tensor
+    :return: Processed image and its mask.
+    """
+    input_image = tf.image.resize(data['image'], (image_size, image_size))
+    input_mask = tf.image.resize(data['segmentation_mask'], (image_size, image_size))
+
+    input_image, input_mask = normalize_images(input_image, input_mask)
+
+    return input_image, input_mask
