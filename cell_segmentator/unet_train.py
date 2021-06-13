5cc2b335669d1e8d359f7d84d40e13b20ed2fecf
m2papierz <m2papierz@gmail.com>
2021-06-13 23:51:29 +0200

    Full training pipeline created

--- cell_segmentator/unet_train.py
+++ cell_segmentator/unet_train.py
@@ -1,33 +1,41 @@
-import unet_model
+import os
 import numpy as np
 import tensorflow as tf
 
 from glob import glob
-from utils import process_train_images
-from sklearn.model_selection import train_test_split
+from unet_model import Unet
+from datetime import datetime
+from utils import process_train_images, config_data_pipeline_performance, DisplayCallback
+from utils import iou, dice, combined_iou_dice_loss
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.callbacks import ModelCheckpoint
 
 # Paths
 DATA_PATH = "D:/DataScience/THESIS/Data/HPA_segmentation/prepared/"
-BEST_MODEL_PATH = ""
+BEST_MODEL_PATH = "/models/"
 TENSORBOARD_LOGS_PATH = ""
 
 # General
 AUTOTUNE = tf.data.experimental.AUTOTUNE
-BATCH_SIZE = 32
-BUFFER_SIZE = 1000
+BATCH_SIZE = 16
+BUFFER_SIZE = 1024
 SEED = 42
 SEGMENTATION_IMAGE_CHANNELS = 3
+EPOCHS = 70
+EARLY_STOP_PATIENCE = 15
+TRAIN_RATIO = 0.7
+VAL_RATIO = 0.15
 
 # Image parameters
 IMAGE_HEIGHT = 512
 IMAGE_WIDTH = 512
-IMAGE_CHANNELS = 4
+IMAGE_CHANNELS = 3
 
-# Network parameters
-LOSS = tf.losses.binary_crossentropy
+# Train parameters
+LOSS = combined_iou_dice_loss
+OPTIMIZER = tf.keras.optimizers.Adam()
+METRICS = [iou, dice]
 
 
 def print_device_info():
@@ -61,76 +69,76 @@ def parse_image(image_path: str) -> dict:
     mask_path = tf.strings.regex_replace(image_path, "image", "mask")
     mask = tf.io.read_file(mask_path)
     mask = tf.image.decode_png(mask, channels=1)
+    mask = tf.where(mask == 255, np.dtype('uint8').type(1), np.dtype('uint8').type(0))
 
     return {'image': image, 'segmentation_mask': mask}
 
 
-def create_dataset(data_path: str) -> dict:
+def create_dataset(data_path: str) -> tuple:
     """
     Creates dataset for image segmentation.
 
     :param data_path: path to data dictionary
-    :return: Dictionary with full data splitted into train, test and validation datasets.
+    :return: Tuple with dataset, train dataset size and validation dataset size
     """
-    dataset_size = len(glob(data_path + "**/*.png"))
+    dataset_size = len(glob(data_path + "image/*.png"))
+    train_dataset_size = int(TRAIN_RATIO * dataset_size)
+    val_dataset_size = int(VAL_RATIO * dataset_size)
+    test_dataset_size = int(VAL_RATIO * dataset_size)
 
-    train_size = int(0.7 * dataset_size)
-    val_size = int(0.15 * dataset_size)
-    test_size = int(0.15 * dataset_size)
+    full_dataset = tf.data.Dataset.list_files(data_path + "image/*.png", seed=SEED)
+    train_dataset = full_dataset.take(train_dataset_size)
+    remaining = full_dataset.skip(train_dataset_size)
+    val_dataset = remaining.take(val_dataset_size)
+    test_dataset = remaining.skip(test_dataset_size)
 
-    full_dataset = tf.data.Dataset.list_files(data_path + "**/*.png", seed=SEED)
-    train_dataset = full_dataset.take(train_size)
-    remaining = full_dataset.skip(train_size)
-    val_dataset = remaining.take(val_size)
-    test_dataset = remaining.skip(test_size)
-
-    train_dataset.map(parse_image)
-    val_dataset.map(parse_image)
+    train_dataset = train_dataset.map(parse_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    val_dataset = val_dataset.map(parse_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
     dataset = {"train": train_dataset, "val": val_dataset, "test": test_dataset}
 
-    # Train dataset
     dataset['train'] = dataset['train'].map(process_train_images, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    dataset['train'] = dataset['train'].shuffle(buffer_size=BUFFER_SIZE, seed=SEED)
-    dataset['train'] = dataset['train'].repeat()
-    dataset['train'] = dataset['train'].batch(BATCH_SIZE)
-    dataset['train'] = dataset['train'].prefetch(buffer_size=AUTOTUNE)
+    dataset['val'] = dataset['val'].map(process_train_images, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
-    # Validation dataset
-    dataset['val'] = dataset['val'].map(process_train_images)
-    dataset['val'] = dataset['val'].repeat()
-    dataset['val'] = dataset['val'].batch(BATCH_SIZE)
-    dataset['val'] = dataset['val'].prefetch(buffer_size=AUTOTUNE)
+    config_data_pipeline_performance(dataset['train'], True, BUFFER_SIZE, BATCH_SIZE, SEED, AUTOTUNE)
+    config_data_pipeline_performance(dataset['val'], False, BUFFER_SIZE, BATCH_SIZE, SEED, AUTOTUNE)
 
-    # Test dataset
-    # TODO: Investigate if test dataset has to be also processed
-    # dataset['test'] = dataset['test'].map(process_train_images)
-    dataset['test'] = dataset['test'].repeat()
-    dataset['test'] = dataset['test'].batch(BATCH_SIZE)
-    dataset['test'] = dataset['test'].prefetch(buffer_size=AUTOTUNE)
-
-    return dataset
+    return dataset, train_dataset_size, val_dataset_size
 
 
-def load_model(img_height, img_width, img_ch):
-    model = unet_model.Unet(img_height, img_width, img_ch)
-    model.model.summary()
+def build_model(img_height: int, img_width: int, img_channels: int, loss: tf.keras.losses.Loss,
+                optimizer: tf.keras.optimizers.Optimizer, metrics: list) -> Unet:
+    model = Unet(img_height, img_width, img_channels)
+    model.compile(loss_function=loss, optimizer=optimizer, metrics=metrics)
     return model
 
 
-def make_callbacks():
+def make_callbacks(sample_images: list, early_stop_patience: int, save_model_path: str):
     callbacks = [
-        EarlyStopping(monitor="val_loss", patience=3, mode="min", verbose=1),
-        # ModelCheckpoint()
+        DisplayCallback(sample_images, displaying_freq=10, enable_displaying=True),
+        EarlyStopping(monitor="val_loss", patience=early_stop_patience, mode="min", verbose=1),
+        ModelCheckpoint(filepath=save_model_path, monitor="val_loss", verbose=1, save_best_only=True)
     ]
     return callbacks
 
 
-def train_model():
-    pass
+def main():
+    print_device_info()
+    segmentation_dataset, train_size, val_size = create_dataset(DATA_PATH)
+    samples = segmentation_dataset['test'].take(1)
+
+    callbacks_list = make_callbacks(sample_images=samples,
+                                    early_stop_patience=EARLY_STOP_PATIENCE,
+                                    save_model_path=BEST_MODEL_PATH)
+
+    unet = build_model(img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT, img_channels=IMAGE_CHANNELS, loss=LOSS,
+                       optimizer=OPTIMIZER, metrics=METRICS)
+
+    with tf.device("device:GPU:0"):
+        unet.train(dataset=segmentation_dataset, train_size=train_size, val_size=val_size, batch_size=BATCH_SIZE,
+                   epochs=EPOCHS, callbacks=callbacks_list)
 
 
 if __name__ == '__main__':
-    print_device_info()
-    Unet_model = load_model(IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
-    # seg_dataset = create_dataset(DATA_PATH)
+    main()
+
