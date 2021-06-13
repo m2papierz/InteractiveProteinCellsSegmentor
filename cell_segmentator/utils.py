b4826afa620cba1a0004eafb1e0dcc9cf7d88a8b
m2papierz <m2papierz@gmail.com>
2021-06-13 22:53:25 +0200

    Loss functions and display callback added

--- cell_segmentator/utils.py
+++ cell_segmentator/utils.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import IPython.display as display
+import tensorflow.keras.backend as K
 
 
 @tf.function
@@ -74,22 +76,15 @@ def config_data_pipeline_performance(dataset: tf.data.Dataset, training: bool, b
     return dataset
 
 
-def display_sample(dataset: tf.data.Dataset) -> None:
+def display_sample_images(images_list: list) -> None:
     """
     Displays sample image and its mask from the dataset.
 
-    :param dataset from which sample images are to be displayed
+    :param images_list: list of images to be displayed
     :return: None
     """
-    sample_image = sample_mask = []
-
-    for image, mask in dataset['train'].take(1):
-        sample_image, sample_mask = image, mask
-
-    images_list = [sample_image[0], sample_mask[0]]
-
     plt.figure(figsize=(18, 18))
-    title = ['Input image', 'True mask']
+    title = ['Input image', 'True mask', 'Prediction mask']
 
     for i in range(len(images_list)):
         plt.subplot(1, len(images_list), i+1)
@@ -97,4 +92,94 @@ def display_sample(dataset: tf.data.Dataset) -> None:
         plt.imshow(tf.keras.preprocessing.image.array_to_img(images_list[i]))
         plt.axis('off')
     plt.show()
+
+
+def show_predictions(model: tf.keras.Model, sample_images: tuple, num=1) -> None:
+    """
+    Show prediction from a sample image.
+
+    :param model: model for predictions
+    :param sample_images: dataset with sample images
+    :param num: number of predictions
+    :return: None
+    """
+    for image, mask in sample_images:
+        pred_mask = model.predict(image)
+        display_sample_images([image[0], mask[0], pred_mask[0]])
+
+
+@tf.function
+def combined_iou_dice_loss(y_true, y_pred, smooth=1, cat_weight=1, iou_weight=1, dice_weight=1):
+    return cat_weight * K.categorical_crossentropy(y_true, y_pred) \
+           + iou_weight * log_iou(y_true, y_pred, smooth) \
+           + dice_weight * log_dice(y_true, y_pred, smooth)
+
+
+@tf.function
+def log_iou(y_true, y_pred, smooth=1):
+    """
+
+    :param y_true:
+    :param y_pred:
+    :param smooth:
+    :return:
+    """
+    return - K.log(iou(y_true, y_pred, smooth))
+
+
+@tf.function
+def log_dice(y_true, y_pred, smooth=1):
+    """
+
+    :param y_true:
+    :param y_pred:
+    :param smooth:
+    :return:
+    """
+    return -K.log(dice(y_true, y_pred, smooth))
+
+
+@tf.function
+def iou(y_true, y_pred, smooth=1):
+    """
+
+    :param y_true:
+    :param y_pred:
+    :param smooth:
+    :return:
+    """
+    intersection = K.sum(K.abs(y_true * y_pred), axis=[1, 2, 3])
+    union = K.sum(y_true, [1, 2, 3]) + K.sum(y_pred, [1, 2, 3]) - intersection
+    return K.mean((intersection + smooth) / (union + smooth), axis=0)
+
+
+@tf.function
+def dice(y_true, y_pred, smooth=1):
+    """
+
+    :param y_true:
+    :param y_pred:
+    :param smooth:
+    :return:
+    """
+    intersection = K.sum(y_true * y_pred, axis=[1, 2, 3])
+    union = K.sum(y_true, axis=[1, 2, 3]) + K.sum(y_pred, axis=[1, 2, 3])
+    return K.mean((2. * intersection + smooth) / (union + smooth), axis=0)
+
+
+class DisplayCallback(tf.keras.callbacks.Callback):
+    """
+    Callbacks displaying sample images and predictions for them each 5th epoch.
+    """
+    def __init__(self, sample_images, displaying_freq=5, enable_displaying=False):
+        super().__init__()
+        self.sample_images = sample_images
+        self.displaying_freq = displaying_freq
+        self.enable_displaying = enable_displaying
+
+    def on_epoch_end(self, epoch, logs=None):
+        display.clear_output(wait=True)
+        if self.enable_displaying:
+            if ((epoch + 1) % self.displaying_freq) == 0:
+                show_predictions(self.model, self.sample_images)
 
