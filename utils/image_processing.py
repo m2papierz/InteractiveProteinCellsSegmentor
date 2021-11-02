9b3e8f09016bd1c76ed7174d1214df1246454d5c
m2papierz <m2papierz@gmail.com>
2021-11-02 13:40:45 +0100

    Image processing functions moved to separate .py file

--- utils/image_processing.py
+++ utils/image_processing.py
@@ -1,38 +1,17 @@
+import numpy as np
 import tensorflow as tf
 
-IMAGE_HEIGHT = 512
-IMAGE_WIDTH = 512
-
 
 @tf.function
-def normalize_images(input_image: tf.Tensor, input_mask: tf.Tensor) -> tuple:
-    """
-    Normalize input tensors. Rescales images pixels values into 0.0 and 1.0 compared to [0,255].
-
-    :param input_image: input image tensor
-    :param input_mask: input mask tensor
-    :return: Normalized tensors.
-    """
-    input_image = tf.cast(input_image, tf.float32) / 255.0
-    return input_image, input_mask
-
-
-@tf.function
-def process_train_images(data: dict) -> tuple:
+def augment_images(data: dict) -> tuple:
     """
     Resize and randomly augment utils.
 
     :param data: dict containing an image and its mask
     :return: Processed imaged and its mask.
     """
-    input_image = tf.image.resize(data['image'], (IMAGE_HEIGHT, IMAGE_WIDTH))
-    input_mask = tf.image.resize(data['segmentation_mask'], (IMAGE_HEIGHT, IMAGE_WIDTH))
-
-    if tf.random.uniform(()) > 0.55:
-        input_image = tf.image.central_crop(input_image, 0.7)
-        input_mask = tf.image.central_crop(input_mask, 0.7)
-        input_image = tf.image.resize(input_image, (512, 512))
-        input_mask = tf.image.resize(input_mask, (512, 512))
+    input_image = data["input"]
+    input_mask = data['mask']
 
     if tf.random.uniform(()) > 0.5:
         input_image = tf.image.random_brightness(input_image, 0.3)
@@ -46,22 +25,36 @@ def process_train_images(data: dict) -> tuple:
         input_image = tf.image.flip_up_down(input_image)
         input_mask = tf.image.flip_up_down(input_mask)
 
-    input_image, input_mask = normalize_images(input_image, input_mask)
-
     return input_image, input_mask
 
 
-@tf.function
-def process_test_images(data: dict) -> tuple:
+def parse_image(image_path: str) -> tuple:
     """
-    Normalize and resize test image and mask tensors.
+    Load an image and its mask.
 
-    :param data: dict containing an image and its mask
-    :return: Processed image and its mask.
+    :param image_path: path to the image
+    :return: Dictionary mapping an image and its mask.
     """
-    input_image = tf.image.resize(data['image'], (IMAGE_HEIGHT, IMAGE_WIDTH))
-    input_mask = tf.image.resize(data['segmentation_mask'], (IMAGE_HEIGHT, IMAGE_WIDTH))
+    image = tf.io.read_file(image_path)
+    image = tf.image.decode_png(image, channels=3)
+    image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+    image = image / 255.0
 
-    input_image, input_mask = normalize_images(input_image, input_mask)
+    pos_click_map = tf.strings.regex_replace(image_path, "image", "pos_click")
+    pos_click_map = tf.io.read_file(pos_click_map)
+    pos_click_map = tf.image.decode_png(pos_click_map, channels=1)
+    pos_click_map = tf.image.convert_image_dtype(pos_click_map, dtype=tf.float32)
 
-    return input_image, input_mask
+    neg_click_map = tf.strings.regex_replace(image_path, "image", "neg_click")
+    neg_click_map = tf.io.read_file(neg_click_map)
+    neg_click_map = tf.image.decode_png(neg_click_map, channels=1)
+    neg_click_map = tf.image.convert_image_dtype(neg_click_map, dtype=tf.float32)
+
+    input_ = tf.concat([image, pos_click_map, neg_click_map], axis=2)
+
+    mask_path = tf.strings.regex_replace(image_path, "image", "mask")
+    mask = tf.io.read_file(mask_path)
+    mask = tf.image.decode_png(mask, channels=1)
+    mask = tf.where(mask > 0, np.dtype('float32').type(1), np.dtype('float32').type(0))
+
+    return input_, mask
