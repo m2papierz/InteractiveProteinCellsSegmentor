0a8bddff2531e0838c03348f828eb91be64d723a
m2papierz <m2papierz@gmail.com>
2021-12-23 23:39:45 +0100

    Minor GUI fixes

--- pipeline/interactive_cell_segmentator.py
+++ pipeline/interactive_cell_segmentator.py
@@ -1,4 +1,3 @@
-import os
 import sys
 import matplotlib
 import numpy as np
@@ -6,7 +5,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import cgitb
 
-from PyQt5 import QtWidgets, QtGui
+from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtWidgets import *
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
@@ -19,14 +18,11 @@ matplotlib.use('Qt5Agg')
 cgitb.enable(format='text')
 
 
-def load_images(path):
-    imgs = []
-    for img in os.listdir(path):
-        image = tf.io.read_file(path + img)
-        image = tf.image.decode_png(image, channels=3)
-        image = tf.image.convert_image_dtype(image, dtype=tf.float32)
-        imgs.append(image)
-    return imgs
+def load_image(path):
+    image = tf.io.read_file(path)
+    image = tf.image.decode_png(image, channels=IMAGE_CHANNELS)
+    image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+    return image
 
 
 def generate_click_maps(pos_clicks, neg_clicks, pos_click_map_scale, neg_click_map_scale):
@@ -56,9 +52,10 @@ def cancat_input_tensors(image, pos_map, neg_map):
 
 
 class MplCanvas(FigureCanvas):
-    def __init__(self, parent=None, width=5, height=5, dpi=100):
-        self.fig = plt.figure(figsize=(width, height), dpi=dpi)
+    def __init__(self, img_width, img_height, dpi):
+        self.fig = plt.figure(figsize=(img_width * dpi, img_height * dpi), dpi=dpi)
         self.axes = self.fig.add_subplot(111)
+        self.fig.subplots_adjust(0, 0, 1, 1)
         super(MplCanvas, self).__init__(self.fig)
 
 
@@ -90,52 +87,62 @@ class CellSegmentator:
         return cut_image[0]
 
 
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, img_width, img_height, imgs_path, img_dpi, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
+class InteractiveCellSegmentator(QtWidgets.QMainWindow):
+    def __init__(self, img_width, img_height, img_path, img_dpi, *args, **kwargs):
+        super(InteractiveCellSegmentator, self).__init__(*args, **kwargs)
 
-        self.images = load_images(path=imgs_path)
+        self.image = load_image(path=img_path)
         self.pos_clicks = []
         self.neg_clicks = []
         self.pos_click = True
 
         # Widgets
-        self.sc = MplCanvas(self, width=img_width, height=img_height, dpi=img_dpi)
+        self.sc = MplCanvas(img_width=img_width, img_height=img_height, dpi=img_dpi)
         self.toolbar = NavigationToolbar(self.sc, self)
-        self.click_typ_button = QRadioButton("Positive click")
-        self.click_typ_button.setChecked(True)
-
-        self.pred_click = QPushButton("Segment cell")
+        self.positive_click_btn = QRadioButton("Positive click")
+        self.negative_click_btn = QRadioButton("Negative click")
+        self.segment_click_btn = QPushButton("Segment cell")
+        self.reset_click_btn = QPushButton("Rest image")
 
         self.__setup_scene()
 
         # Interactions
-        self.sc.mpl_connect("button_press_event", self.__on_click)
-        self.click_typ_button.toggled.connect(lambda: self.__on_click_type_change())
-        self.pred_click.clicked.connect(lambda: self.__on_click_segment())
-
-        self.sc.axes.imshow(self.images[5])
-        self.show()
+        self.sc.mpl_connect("button_press_event", self.__on_image_click)
+        self.positive_click_btn.toggled.connect(lambda: self.__on_click_type_change_click())
+        self.negative_click_btn.toggled.connect(lambda: self.__on_click_type_change_click())
+        self.segment_click_btn.clicked.connect(lambda: self.__on_segment_click())
+        self.reset_click_btn.clicked.connect(lambda: self.__on_reset_click())
 
     def __setup_scene(self):
         layout = QVBoxLayout()
         layout.addWidget(self.toolbar)
         layout.addWidget(self.sc)
-        layout.addWidget(self.click_typ_button)
-        layout.addWidget(self.pred_click)
-        widget = QWidget()
+        layout.addWidget(self.positive_click_btn)
+        layout.addWidget(self.negative_click_btn)
+        layout.addWidget(self.segment_click_btn)
+        layout.addWidget(self.reset_click_btn)
+        widget = QWidget(flags=QtCore.Qt.WindowCloseButtonHint | QtCore.Qt.WindowMinimizeButtonHint)
         widget.setLayout(layout)
         self.setCentralWidget(widget)
+
+        self.positive_click_btn.setFont(QtGui.QFont('Arial', 12))
+        self.negative_click_btn.setFont(QtGui.QFont('Arial', 12))
+        self.segment_click_btn.setFont(QtGui.QFont('Arial', 12))
+        self.reset_click_btn.setFont(QtGui.QFont('Arial', 12))
+
+        self.positive_click_btn.setChecked(False)
+        self.negative_click_btn.setChecked(False)
 
         self.sc.axes.get_xaxis().set_visible(False)
         self.sc.axes.get_yaxis().set_visible(False)
 
-        if self.pos_click:
-            self.sc.setCursor(QtGui.QCursor(QtGui.QPixmap("utils/pyqt_cursor_imgs/green.png")))
-        else:
-            self.sc.setCursor(QtGui.QCursor(QtGui.QPixmap("utils/pyqt_cursor_imgs/red.png")))
+        self.sc.axes.imshow(self.image)
+        self.show()
 
-    def __on_click(self, event):
+    def __on_image_click(self, event):
+        if not self.positive_click_btn.isChecked() and not self.negative_click_btn.isChecked():
+            return
+
         if event.xdata is None or event.ydata is None:
             return
 
@@ -146,21 +153,35 @@ class MainWindow(QtWidgets.QMainWindow):
             self.neg_clicks.append([int(event.xdata), int(event.ydata)])
             print(f'x: {event.xdata} y: {event.ydata}')
 
-    def __on_click_type_change(self):
-        if self.click_typ_button.isChecked():
+    def __on_click_type_change_click(self):
+        if self.positive_click_btn.isChecked():
             self.pos_click = True
             self.sc.setCursor(QtGui.QCursor(QtGui.QPixmap("utils/pyqt_cursor_imgs/green.png")))
+            self.negative_click_btn.setChecked(False)
         else:
             self.pos_click = False
             self.sc.setCursor(QtGui.QCursor(QtGui.QPixmap("utils/pyqt_cursor_imgs/red.png")))
+            self.positive_click_btn.setChecked(False)
 
-    def __on_click_segment(self):
+    def __on_segment_click(self):
         cell_segmentator.pos_clicks = self.pos_clicks
         cell_segmentator.neg_clicks = self.neg_clicks
-        segmented_cell = cell_segmentator.segment(np.array(self.images[5]))
+        segmented_cell = cell_segmentator.segment(np.array(self.image))
 
+        self.pos_clicks = []
+        self.neg_clicks = []
+
+        self.sc.axes.cla()
         self.sc.axes.imshow(segmented_cell)
-        self.show()
+        self.sc.fig.canvas.draw_idle()
+
+    def __on_reset_click(self):
+        self.pos_clicks = []
+        self.neg_clicks = []
+
+        self.sc.axes.cla()
+        self.sc.axes.imshow(self.image)
+        self.sc.fig.canvas.draw_idle()
 
 
 if __name__ == '__main__':
@@ -170,20 +191,25 @@ if __name__ == '__main__':
     MODELS_PATH = config["MODELS_PATH"]
     UNET_DP_MODEL_PATH = config["UNET_DP_MODEL_PATH"]
     DATA_PATH_TEST = config["DATA_PATH_TEST"]
-
+    TEST_IMAGE_PATH = config['TEST_IMAGE_PATH']
     MODEL_PATH = PROJECT_PATH + MODELS_PATH + UNET_DP_MODEL_PATH
-    IMAGES_PATH = PROJECT_PATH + DATA_PATH_TEST + "image/"
 
     IMAGE_HEIGHT = config["IMAGE_HEIGHT"]
     IMAGE_WIDTH = config["IMAGE_WIDTH"]
+    IMAGE_CHANNELS = config["IMAGE_CHANNELS"]
     POS_CLICK_MAP_SCALE = config["POS_CLICK_MAP_SCALE"]
     NEG_CLICK_MAP_SCALE = config["NEG_CLICK_MAP_SCALE"]
 
+    WINDOW_X = config["WINDOW_X"]
+    WINDOW_Y = config["WINDOW_Y"]
+    WINDOW_SIZE = config["WINDOW_SIZE"]
+    IMG_DPI = config["IMG_DPI"]
+
     cell_segmentator = CellSegmentator(model_path=MODEL_PATH, pos_clicks=None, neg_clicks=None)
     app = QtWidgets.QApplication(sys.argv)
-    w = MainWindow(img_width=10,
-                   img_height=10,
-                   img_dpi=300,
-                   imgs_path=IMAGES_PATH)
-    w.setGeometry(700, 200, 1200, 1200)
+    ics = InteractiveCellSegmentator(img_width=IMAGE_WIDTH,
+                                     img_height=IMAGE_HEIGHT,
+                                     img_dpi=IMG_DPI,
+                                     img_path=TEST_IMAGE_PATH)
+    ics.setGeometry(WINDOW_X, WINDOW_Y, WINDOW_SIZE, WINDOW_SIZE)
     app.exec_()
