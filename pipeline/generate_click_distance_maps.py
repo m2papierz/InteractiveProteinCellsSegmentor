23e6039a047726f193605fc95fcf5c270161878d
m2papierz <m2papierz@gmail.com>
2022-01-30 21:27:39 +0100

    Attention module introduced

--- pipeline/generate_click_distance_maps.py
+++ pipeline/generate_click_distance_maps.py
@@ -23,9 +23,9 @@ def euclidean_distance(p1: tuple, p2: tuple, scale=1.0) -> float:
     return np.sqrt(distance) * scale
 
 
-def create_gaussian_distance_map(shape: tuple, points: list, scale=1.0, image=False, omega=255.0) -> np.array:
+def create_guidance_map(shape: tuple, points: list, scale=1.0, image=False, omega=255.0) -> np.array:
     """
-    Creates gaussian distance map for given points.
+    Creates guidance map for given points being scaled reverted Euclidean distance map.
 
     :param shape: shape of the output map
     :param points: list of points coordinates
@@ -72,7 +72,7 @@ def create_gaussian_distance_map(shape: tuple, points: list, scale=1.0, image=Fa
 
 def get_annotations_dict(data_xml: BeautifulSoup) -> dict:
     """
-    Creates dictionaries with points annotations.
+    Creates dictionaries with point annotations.
 
     :param data_xml: BeautifulSoup instance of xml file with annotations
     :return: tuple of dictionaries with train and test point annotations
@@ -80,7 +80,6 @@ def get_annotations_dict(data_xml: BeautifulSoup) -> dict:
     images = data_xml.find_all("image")
 
     ann_dict = {}
-
     for image in images:
         image_dict = {}
         for i, point in enumerate(image.find_all("points")):
@@ -91,9 +90,9 @@ def get_annotations_dict(data_xml: BeautifulSoup) -> dict:
     return ann_dict
 
 
-def create_and_save_distance_maps(xml_files_path: str, pos_save: str, neg_save: str) -> None:
+def create_and_save_guidance_maps(xml_files_path: str, pos_save: str, neg_save: str) -> None:
     """
-    Creates and saves gaussian distance maps of positive and negative clicks for given annotations.
+    Creates and saves guidance maps images of positive and negative clicks for given annotations.
 
     :param xml_files_path: dictionary with points annotations
     :param pos_save: path to save positive clicks maps
@@ -102,7 +101,6 @@ def create_and_save_distance_maps(xml_files_path: str, pos_save: str, neg_save: 
     for i, xml_file in enumerate(os.listdir(xml_files_path)):
         with open(xml_files_path + xml_file, 'r') as xml:
             data = xml.read()
-        xml.close()
         ann_dict = get_annotations_dict(BeautifulSoup(data, "xml"))
 
         for filename, points_dict in tqdm(ann_dict.items(), total=len(ann_dict), file=sys.stdout):
@@ -115,10 +113,10 @@ def create_and_save_distance_maps(xml_files_path: str, pos_save: str, neg_save: 
                 if "neg_click" in label:
                     neg_coordinates.append(coordinates)
 
-            pos_map = create_gaussian_distance_map((IMAGE_HEIGHT, IMAGE_WIDTH), pos_coordinates,
-                                                   image=False, scale=POS_CLICK_MAP_SCALE)
-            neg_map = create_gaussian_distance_map((IMAGE_HEIGHT, IMAGE_WIDTH), neg_coordinates,
-                                                   image=False, scale=NEG_CLICK_MAP_SCALE)
+            pos_map = create_guidance_map(shape=(IMAGE_HEIGHT, IMAGE_WIDTH), points=pos_coordinates,
+                                          image=True, scale=POS_CLICK_MAP_SCALE)
+            neg_map = create_guidance_map(shape=(IMAGE_HEIGHT, IMAGE_WIDTH), points=neg_coordinates,
+                                          image=True, scale=NEG_CLICK_MAP_SCALE)
 
             Image.fromarray(pos_map.astype(np.uint8)).save(pos_save + filename, "PNG")
             Image.fromarray(neg_map.astype(np.uint8)).save(neg_save + filename, "PNG")
@@ -143,12 +141,12 @@ if __name__ == '__main__':
     POS_CLICK_MAP_SCALE = config["POS_CLICK_MAP_SCALE"]
     NEG_CLICK_MAP_SCALE = config["NEG_CLICK_MAP_SCALE"]
 
-    print("\n----- GENERATING TRAIN DISTANCE MAPS -----")
-    create_and_save_distance_maps(xml_files_path=ANNOTATIONS_XML_TRAIN_PATH,
+    print("\n----- GENERATING TRAIN GUIDANCE MAPS -----")
+    create_and_save_guidance_maps(xml_files_path=ANNOTATIONS_XML_TRAIN_PATH,
                                   pos_save=POS_CLICK_MAPS_TRAIN_PATH,
                                   neg_save=NEG_CLICK_MAPS_TRAIN_PATH)
 
-    print("\n----- GENERATING TEST DISTANCE MAPS -----")
-    create_and_save_distance_maps(xml_files_path=ANNOTATIONS_XML_TEST_PATH,
+    print("\n----- GENERATING TEST GUIDANCE MAPS -----")
+    create_and_save_guidance_maps(xml_files_path=ANNOTATIONS_XML_TEST_PATH,
                                   pos_save=POS_CLICK_MAPS_TEST_PATH,
                                   neg_save=NEG_CLICK_MAPS_TEST_PATH)
---
+++ unet_architectures/attention_module.py
@@ -0,0 +1,60 @@
+import tensorflow.keras.backend as K
+
+from tensorflow.keras.layers import Dense, Conv2D, Add, Activation, GlobalMaxPooling2D, GlobalAveragePooling2D
+from tensorflow.keras.layers import multiply, Reshape, Concatenate, Lambda
+
+
+def channel_attention(input_feature, ratio):
+    """
+    Channel Attention Module.
+
+    :param input_feature: input feature maps
+    :param ratio: the ration of input feature maps and filters in first dense layer of the module
+    :return: channel-refined feature map
+    """
+    channel = input_feature.shape[-1]
+    shared_layer_one = Dense(channel // ratio, kernel_initializer='he_normal', use_bias=True)
+    shared_layer_two = Dense(channel, kernel_initializer='he_normal', use_bias=True, bias_initializer='zeros')
+
+    avg_pool = GlobalAveragePooling2D()(input_feature)
+    avg_pool = Reshape((1, 1, channel))(avg_pool)
+    avg_pool = shared_layer_one(avg_pool)
+    avg_pool = shared_layer_two(avg_pool)
+
+    max_pool = GlobalMaxPooling2D()(input_feature)
+    max_pool = Reshape((1, 1, channel))(max_pool)
+    max_pool = shared_layer_one(max_pool)
+    max_pool = shared_layer_two(max_pool)
+
+    channel_feature = Add()([avg_pool, max_pool])
+    channel_feature = Activation('sigmoid')(channel_feature)
+
+    return multiply([input_feature, channel_feature])
+
+
+def spatial_attention(input_feature):
+    """
+    Spatial Attention Module.
+
+    :param input_feature: input feature maps
+    :return: spatial-refined feature map
+    """
+    avg_pool = Lambda(lambda x: K.mean(x, axis=3, keepdims=True))(input_feature)
+    max_pool = Lambda(lambda x: K.max(x, axis=3, keepdims=True))(input_feature)
+    concat = Concatenate(axis=3)([avg_pool, max_pool])
+    spatial_feature = Conv2D(filters=1, kernel_size=7, strides=1, padding='same', activation='sigmoid',
+                             kernel_initializer='he_normal', use_bias=False)(concat)
+    return multiply([input_feature, spatial_feature])
+
+
+def conv_block_attention_module(input_feature, ratio=8):
+    """
+    Convolutional Block Attention Module proposed in: https://arxiv.org/abs/1807.06521
+
+    :param input_feature: input feature maps
+    :param ratio: the ration of input feature maps and filters in first dense layer of the channel module
+    :return: refined feature map
+    """
+    refined_feature = channel_attention(input_feature, ratio)
+    refined_feature = spatial_attention(refined_feature)
+    return refined_feature
